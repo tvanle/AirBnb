eb31d65b46aa9d5b7290b5bf296c57b2a6d14ad6
Trong Le <110284635+TVanLe@users.noreply.github.com>
2025-02-16 16:39:09 +0700

    update navbar

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+﻿const Navbar = () => {
+    return (
+        <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-4 py-3 bg-white dark:bg-black/[.9] border-b border-solid border-black/[.08] dark:border-white/[.145]">
+            <div></div>
+        </div>
+    );
+}
+
+export default Navbar;
