e43e5ae8ed571fd59a1465f1794247805605c01b
Trong Le <trongthptdqh@gmail.com>
2025-05-04 10:27:16 +0700

    Update upload preset in ImageUpload component and remove status field from schema

--- components/inputs/ImageUpload.tsx
+++ components/inputs/ImageUpload.tsx
@@ -25,7 +25,7 @@ function ImageUpload({ onChange, value }: Props) {
   return (
     <CldUploadWidget
       onUpload={handleCallback}
-      uploadPreset="cptcecyi"
+      uploadPreset="test123"
       options={{
         maxFiles: 1,
       }}
